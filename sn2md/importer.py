f25c6a4f1f7d78c2d7da813e4ca9c895f0ea0194
Dane Summers <dsummersl@yahoo.com>
2025-02-20 16:10:05 -0500

    Move metadata functions to their own module.

--- sn2md/importer.py
+++ sn2md/importer.py
@@ -1,11 +1,9 @@
 import base64
 import uuid
-import hashlib
 import logging
 import os
 from datetime import datetime
 
-import yaml
 from jinja2 import Template
 
 from sn2md.ai_utils import image_to_markdown, image_to_text
@@ -13,61 +11,11 @@ from sn2md.importers.pdf import PDFExtractor
 from sn2md.importers.png import PNGExtractor
 from sn2md.types import Config, ImageExtractor
 from sn2md.importers.note import NotebookExtractor, convert_binary_to_image
-from .types import ConversionMetadata
+from sn2md.metadata import (check_metadata_file, write_metadata_file)
 
 from tqdm import tqdm
 
 logger = logging.getLogger(__name__)
-
-
-def check_metadata_file(metadata_file: str) -> ConversionMetadata | None:
-    """Check the hashes of the source file against the metadata.
-
-    Raises a ValueError if the source file hasn't been modified.
-
-    Returns the computed source and output hashes.
-    """
-    metadata_path = os.path.join(metadata_file, ".sn2md.metadata.yaml")
-    if os.path.exists(metadata_path):
-        with open(metadata_path, "r") as f:
-            data = yaml.safe_load(f)
-            metadata = ConversionMetadata(**data)
-
-            with open(metadata.output_file, "rb") as f:
-                output_hash = hashlib.sha1(f.read()).hexdigest()
-
-            with open(metadata.input_file, "rb") as f:
-                source_hash = hashlib.sha1(f.read()).hexdigest()
-
-            if metadata.input_hash == source_hash:
-                raise ValueError(f"{metadata.input_file} hasn't changed!")
-
-            if metadata.output_hash != output_hash:
-                raise ValueError(f"{metadata.output_file} has been changed!")
-
-            return metadata
-
-
-def write_metadata_file(source_file: str, output_file: str) -> None:
-    """Write the source hash and path to the metadata file."""
-    output_path = os.path.dirname(output_file)
-    with open(output_file, "rb") as f:
-        output_hash = hashlib.sha1(f.read()).hexdigest()
-
-    with open(source_file, "rb") as f:
-        source_hash = hashlib.sha1(f.read()).hexdigest()
-
-    metadata_path = os.path.join(output_path, ".sn2md.metadata.yaml")
-    with open(metadata_path, "w") as f:
-        yaml.dump(
-            ConversionMetadata(
-                input_file=source_file,
-                input_hash=source_hash,
-                output_file=output_file,
-                output_hash=output_hash,
-            ),
-            f,
-        )
 
 
 def import_supernote_file_core(
---
+++ sn2md/metadata.py
@@ -0,0 +1,56 @@
+import hashlib
+import os
+import yaml
+from .types import ConversionMetadata
+
+
+def check_metadata_file(metadata_file: str) -> ConversionMetadata | None:
+    """Check the hashes of the source file against the metadata.
+
+    Raises a ValueError if the source file hasn't been modified.
+
+    Returns the computed source and output hashes.
+    """
+    metadata_path = os.path.join(metadata_file, ".sn2md.metadata.yaml")
+    if os.path.exists(metadata_path):
+        with open(metadata_path, "r") as f:
+            data = yaml.safe_load(f)
+            metadata = ConversionMetadata(**data)
+
+            with open(metadata.output_file, "rb") as f:
+                output_hash = hashlib.sha1(f.read()).hexdigest()
+
+            with open(metadata.input_file, "rb") as f:
+                source_hash = hashlib.sha1(f.read()).hexdigest()
+
+            if metadata.input_hash == source_hash:
+                raise ValueError(f"{metadata.input_file} hasn't changed!")
+
+            if metadata.output_hash != output_hash:
+                raise ValueError(f"{metadata.output_file} has been changed!")
+
+            return metadata
+
+
+def write_metadata_file(source_file: str, output_file: str) -> None:
+    """Write the source hash and path to the metadata file."""
+    output_path = os.path.dirname(output_file)
+    with open(output_file, "rb") as f:
+        output_hash = hashlib.sha1(f.read()).hexdigest()
+
+    with open(source_file, "rb") as f:
+        source_hash = hashlib.sha1(f.read()).hexdigest()
+
+    metadata_path = os.path.join(output_path, ".sn2md.metadata.yaml")
+    with open(metadata_path, "w") as f:
+        yaml.dump(
+            ConversionMetadata(
+                input_file=source_file,
+                input_hash=source_hash,
+                output_file=output_file,
+                output_hash=output_hash,
+            ),
+            f,
+        )
+
+
